a96360541f01a601b56ebe586e6f6baaf43132f2
Preet2099x <preet2099x@gmail.com>
2025-01-31 09:23:29 +0530

    cleaned code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/navbar/Navbar'; // Add this
+import Navbar from './components/navbar/Navbar'; 
 import SearchPage from './components/searchPage/SearchPage';
 import CoursesPage from './components/coursesPage/CoursesPage';
 import './App.css';
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Navbar /> 
-      <main style={{ maxWidth: '1200px', margin: '0 auto', padding: '2rem' }}>
+      <main>
         <Routes>
           <Route path="/" element={<SearchPage />} />
           <Route path="/courses" element={<CoursesPage />} />
